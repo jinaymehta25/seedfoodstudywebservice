37afa216ec2d60d755988fc2f504c96d308fd602
Jinay Mehta <mjinay@gmail.com>
2014-08-15 11:32:39 -0700

    Update setup.py
    Update pandas

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='seedfoodstudywebservice',
       version='1.0',
       description='OpenShift App',
-      author='Sara Queen',
-      author_email='saragleequeen@gmail.com',
+      author='Jinay Mehta',
+      author_email='jinaymehta25@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.7.2', 'configparser','numpy>=1.8.1','pandas'],
+      install_requires=['Flask>=0.7.2', 'configparser','numpy>=1.8.1','pandas>=0.14.1'],
      )
